374b91a51ac03607bde20bd9b7b815a7f4890310
ForgacsAlex012 <forgacs.alex@diak.szbi-pg.hu>
2024-11-12 10:35:40 +0100

    formpizza

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ def index():
 
 @app.route('/pizza-order', methods=["GET"])
 def pizza_order():
+    if request.method =="POST":
+        _name = request.form.get("name")
+        _phone = request.form.get("phone")
+        _size = request.form.get("size")
+        order ={
+            "name": _name,
+            "phone": _phone,
+            "size": _size}
+        orders.append(order)
+        print(orders)
     return render_template('pizza_order.html')
 
 if __name__ == "__main__":
